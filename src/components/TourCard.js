e90ef9b216fa7e931cd6ec64b8f038d136ac5e84
jinh013 <jmrandrianant@gmail.com>
2022-08-29 20:18:54 +0300

    we used here the an element of the SURFACE component of MUI called PAPER

---
+++ src/components/TourCard.js
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material';
+import React from 'react'
+
+function TourCard() {
+  return (
+    <div>
+        <Paper elevation={3} variant='outlined'>Hello</Paper>
+    </div>
+  )
+}
+
+export default TourCard;
