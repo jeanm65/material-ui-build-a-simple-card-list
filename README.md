8af6d0d09ba4d2edb5a393254c764aa863dc27b0
jinh013 <jmrandrianant@gmail.com>
2022-08-30 08:56:42 +0300

    we largely using the TYPOGRAPHY, RATING components in this section, the all wrapped inside the BOX component which is the equivalent of div in css

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 1- to display cards, we can use the Surface category of mui: we can use the components: CARD or PAPER, we use PAPER here;
-2- About Layout, we can use the Layout category of mui: Box and Container are the most basic layout component,we use Container here;
-3- The Container centers your content horizontally. It's the most basic layout element.
-4- One type of layout is the Grid component, we often use it;
-5- as Components, they can get props like spacing, etc;
+2- About Layout, we can use the Layout category of mui: BOX and CONTAINER are the most basic layout component,we use Container here;
+3- The CONTAINER centers your content horizontally. It's the most basic layout element.
+4- One type of layout is the GRID component, we often use it;
+5- as Components, they can get props like spacing, etc; WE CAN ALSO USE INLINE STYLE LIKE IN CSS with the props "sx";
 6- the number of x-column that can be supported by Grid is 12 (xs={3} can give 4 cards on the same line)
-5-
+5- We as well use a data display category of mui which is the compoenent TYPOGRAPHY  (to write text) 
+6- we also installed mui-icons, to get access to the AccessTime icons;
+7- we also use the input category of mui, to display rating (appear as stars), we use the RATING component of mui.
+8-
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,11 +1,52 @@
-import { Grid, Paper } from '@mui/material';
+import { Box, Grid, Paper } from '@mui/material';
 import React from 'react'
+import Typography from '@mui/material/Typography';
+import {AccessTime} from '@mui/icons-material';
+import Rating from '@mui/material/Rating';
 
 function TourCard() {
   return (
     <Grid item xs={3}>
         <Paper elevation={3}>
-            Hello
+           <img src='https://media.istockphoto.com/photos/beautiful-landscape-with-waterfall-in-northern-forest-on-summer-of-picture-id1366230496?b=1&k=20&m=1366230496&s=170667a&w=0&h=GySyxgFUa2Elmgq6xsg-bIACQ3fEDGnRZIgXRV16dJk=' alt='' className='img'/>
+           <Box paddingX={1}>
+                <Typography variant='subtitle1 ' component='h2' marginBottom={2}>
+                    Immerse into the falls
+                </Typography>
+                <Box
+           sx={{
+            display: 'flex',
+            alighItems: 'center',
+           }}>
+                <AccessTime sx={{ width: 12.5 }} />
+                <Typography variant='body2' component='p' marginLeft={0.5}>
+                    5hours
+                </Typography>
+                </Box>
+                <Box 
+                sx={{
+                  display: 'flex',
+                  alighItems: 'center',
+                 }}
+                 marginTop={3}>
+                     <Rating name="read-only" value={4.5} precision={0.5} size="small" readOnly />
+                        <Typography variant='body2' component='p' marginLeft={0.5}>
+                            4.5
+                        </Typography>
+                        <Typography variant='body3' component='p' marginLeft={0.5} marginTop={0}>
+                            (655 reviews)
+                        </Typography>     
+                 </Box>
+                 <Box>
+                    <Typography variant="h6" component="h3" marginTop={0}>
+                        From C $450
+                    </Typography>
+                 </Box>
+                 <Box>
+
+                 </Box>
+           </Box>
+           
         </Paper>
     </Grid>
   )
